2d0d71734d96be4788ac5049f055ba1f0ac9f888
abbas farahani <abbasfarahani4846@outlook.com>
2024-07-10 12:28:12 +0330

    list products

---
+++ Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.Models.db;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly OnlineShopContext _context;
+        public ProductController(OnlineShopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _context.Products.OrderByDescending(x => x.Id).ToList();
+            return View(products);
+        }
+        //public IActionResult SingleProduct(int productId)
+        //{
+        //    Product product = _context.Products.FirstOrDefault(product => product.Id == productId);
+
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    ViewData["gallery"] = _context.ProductGaleries.Where(x => x.ProductId == productId).ToList();
+        //    return View(product);
+        //}
+    }
+}
