7b1e763b55ec37d846e36360cbe813e5d00e4c1b
abbas farahani <abbasfarahani4846@outlook.com>
2024-07-24 11:46:13 +0330

    Product comment

--- Ecommerce/Controllers/ProductController.cs
+++ Ecommerce/Controllers/ProductController.cs
@@ -3,6 +3,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+using System.Text.RegularExpressions;
+
 namespace Ecommerce.Controllers
 {
     public class ProductController : Controller
@@ -44,8 +46,40 @@ namespace Ecommerce.Controllers
 
             ViewData["gallery"] = _context.ProductGaleries.Where(x => x.ProductId == id).ToList();
 
-            ViewData["newProducts"] = _context.Products.Where(x => x.Id != id).Take(6).OrderByDescending(x=>x.Id).ToList();
+            ViewData["newProducts"] = _context.Products.Where(x => x.Id != id).Take(6).OrderByDescending(x => x.Id).ToList();
+
+            ViewData["comments"] = _context.Comments.Where(x => x.ProductId == id).OrderByDescending(x => x.CreateDate).ToList();
             return View(product);
+        }
+        [HttpPost]
+        public IActionResult SubmitComment(string name, string email, string comment, int productId)
+        {
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(comment) && productId != 0)
+            {
+                Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                Match match = regex.Match(email);
+                if (!match.Success)
+                {
+                    TempData["ErrorMessage"] = "Email is not valid";
+                    return RedirectToAction("ProductDetails", new { id = productId });
+                }
+
+                Comment newComment = new Comment();
+                newComment.Name = name;
+                newComment.Email = email;
+                newComment.CommentText = comment;
+                newComment.ProductId = productId;
+                newComment.CreateDate = DateTime.Now;
+
+                _context.Comments.Add(newComment);
+                _context.SaveChanges();
+
+                TempData["SuccessMessage"] = "Youre comment submited success fully";
+                return RedirectToAction("ProductDetails", new { id = productId });
+            }
+
+            TempData["ErrorMessage"] = "Please complete youre information";
+            return RedirectToAction("ProductDetails", new { id = productId });
         }
 
         // public IActionResult ProductsByCategory(string? category)
