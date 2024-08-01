c4cf75ede86e8fcb8de6f75b452d27620398bfd3
abbas farahani <abbasfarahani4846@outlook.com>
2024-08-01 14:26:31 +0330

    add to cart

--- Ecommerce/Controllers/CartController.cs
+++ Ecommerce/Controllers/CartController.cs
@@ -1,9 +1,11 @@
 ﻿using Ecommerce.Models.db;
+using Ecommerce.Models.ViewModels;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Ecommerce.Controllers
@@ -19,22 +21,93 @@ namespace Ecommerce.Controllers
         {
             return View();
         }
-        //[HttpPost]
-        //public IActionResult AddToCart(int productId, int count)
-        //{
-        //    if (count == 0)
-        //    {
-        //        return Redirect("/");
-        //    }
+        /// <summary>
+        /// افزودن یا بروزرسانی سبد خرید
+        /// </summary>
+        /// <param name="productId">ایدی محصول</param>
+        /// <param name="count">تعداد مورد نظر. اگر صفر باشه یعنی قصد حذف رو داره. این مورد دستی توسط خودمون کنترل میشه</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult UpdateCart(int productId, int count)
+        {
+            if (count == 0)
+            {
+                return Redirect("/");
+            }
 
-        //    var product = _context.Products.FirstOrDefault(x => x.Id == productId);
-        //    if (product == null)
-        //    {
-        //        return Redirect("/");
-        //    }
+            var product = _context.Products.FirstOrDefault(x => x.Id == productId);
+            if (product == null)
+            {
+                return Redirect("/");
+            }
+            //گرفتن لیست محصولات در سبد از تابع مخصوص اینکار
+            var cartItems = GetCartItems();
 
-        //    HttpContext.Session.Set<List<SalesCartItemsModel>>("cartItems", itemsList);
-        //    var value = HttpContext.Session.Get<List<SalesCartItemsModel>>("cartItems");
-        //}
+            var foundProductInCart = cartItems.FirstOrDefault(x => x.ProductId == productId);
+
+            //اگر محصول پیدا بشه یعنی تو سبدش هست و قصد داره تعداد  خرید رو تغییر بده
+            if (foundProductInCart != null)
+            {
+                // اگر بزرگتر از صفر باشه یعنی قصد بروز رسانی تعداد رو در غیر اینصورت از سبد حذف میشه.
+                if (count > 0)
+                {
+                    foundProductInCart.Count = count;
+                }
+                else
+                {
+                    cartItems.Remove(foundProductInCart);
+                }
+
+            }
+            else
+            {
+                var newCartItem = new CartViewModel() { };
+                newCartItem.ProductId = productId;
+                newCartItem.Count = count;
+
+                cartItems.Add(newCartItem);
+            }
+
+
+            return new JsonResult(cartItems.Count());
+        }
+        public IActionResult GetSmallCart()
+        {
+            var cartItems = GetCartItems();
+
+            if (cartItems.Any() == false)
+            {
+                return View();
+            }
+
+            var cartItemProductIds = cartItems.Select(x => x.ProductId);
+
+            var products = _context.Products.Where(p => cartItemProductIds.Contains(p.Id)).Select(ps => new ProductCartViewModel
+            {
+                Id = ps.Id,
+                ImageName = ps.ImageName,
+                Price = ps.Price - ps.Discount ?? 0,
+                Title = ps.Title,
+                Count = cartItems.Single(x => x.ProductId == ps.Id).Count,
+                RowSumPrice = (ps.Price - ps.Discount ?? 0) * cartItems.Single(x => x.ProductId == ps.Id).Count,
+            }).ToList();
+
+            return PartialView("_SmallCartPartialView", products);
+        }
+
+        public List<CartViewModel> GetCartItems()
+        {
+            List<CartViewModel> cartList = new List<CartViewModel>();
+
+            var prevCartItemsString = Request.Cookies["Cart"];
+
+            //اگر نال نباشه یعنی سبدش خالی نیست و باید تبدیلش کنیم به لیستی از ویو مدل در غیر اینصورت لیست خالی سبد بر میگردونیم
+            if (!string.IsNullOrEmpty(prevCartItemsString))
+            {
+                cartList = JsonConvert.DeserializeObject<List<CartViewModel>>(prevCartItemsString);
+            }
+
+            return cartList;
+        }
     }
 }
---
+++ Ecommerce/Models/ViewModels/ProductCartViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecommerce.Models.ViewModels
+{
+    public class ProductCartViewModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ImageName { get; set; }
+        public int Count { get; set; }
+        public int Price { get; set; }
+        public int RowSumPrice  { get; set; }
+    }
+}
